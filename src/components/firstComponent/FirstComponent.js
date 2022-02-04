dad56236cd4360fa010103cabd4394c8560d873d
unknown <mail.ahliwasum@gmail.com>
2022-02-04 22:10:46 +0530

    adding basic styling

--- src/components/firstComponent/FirstComponent.js
+++ src/components/firstComponent/FirstComponent.js
@@ -1,5 +1,14 @@
-const FirstComponent=()=>{
-    return <h1>I am First Component</h1>
+import "./FirstComponent.css";
+const FirstComponent = () => {
+  return (
+    <div className="first-item">
+      <div>27th Feb 2022</div>
+      <div className="first-item__description">
+        <h2>I am First Item</h2>
+        <div className="first-item__price">2575.00</div>
+      </div>
+    </div>
+  );
 };
 
 export default FirstComponent;
