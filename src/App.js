3b177ea5f7953d9a8d45f5e8d2ab4234c79c606f
unknown <mail.ahliwasum@gmail.com>
2022-02-04 22:01:54 +0530

    first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import FirstComponent from './components/firstComponent/FirstComponent';
 
 const App=()=> {
   return (
     <div className="App">
        <h1>Hello React</h1>
+       <FirstComponent />
     </div>
   );
 }
---
+++ src/components/firstComponent/FirstComponent.js
@@ -0,0 +1,5 @@
+const FirstComponent=()=>{
+    return <h1>I am First Component</h1>
+};
+
+export default FirstComponent;
